90e4ee430b1f2a8972bd1bdf80bde76049c04343
YumrukPaketcisi <kbasarkocabas@gmail.com>
2023-04-30 13:58:40 +0300

    Score.java kart puan fonksiyon ismi
    değişti.
    extra olarak string equals kullandım hatalıydı. Barış adamlığı

--- Score.java
+++ Score.java
@@ -1,16 +1,16 @@
 import java.util.ArrayList;
 
 public class Score {
-    private ArrayList<String> cardvalues = new ArrayList<String>(); //valuable cards specified in points.txt
+    private ArrayList<String> cardsValues = new ArrayList<String>(); //valuable cards specified in points.txt
 
     public static String[] readFile() {}
-    public int  card_point(String card){
+    public int  getCardPoint(String card){
         String valuable_card  = null;    //one of cards in cardvalues
         int card_value = 0;    //value of that card.
-        for(String i: cardvalues){
+        for(String i: cardsValues){
             valuable_card = i.split(" ")[0];
             card_value = Integer.parseInt(i.split(" ")[1]);
-            if(card == valuable_card) return card_value;  
+            if(card.equals(valuable_card)) return card_value;  
         }
         return 1;
     }
