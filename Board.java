7b996c2bda01876de97726e6c744683560b2c521
ngner046 <94244632+ngner046@users.noreply.github.com>
2023-04-30 14:10:56 +0300

    Update Board.java
    Board's constructor and a getter method have been added

--- Board.java
+++ Board.java
@@ -4,8 +4,10 @@ public class Board {
     private ArrayList<String> boardCards;
 
     public Board() {
-
+        this.boardCards = new ArrayList<String>();
     }
+
+    public ArrayList<String> getBoardCards() {return this.boardCards;}
 
     public void addCard(String card) {this.boardCards.add(card);}
     public void clearBoard() {this.boardCards.clear();}
