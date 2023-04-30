8e38e70856cbffa91563858e038b72da3c873d2e
ngner046 <94244632+ngner046@users.noreply.github.com>
2023-04-30 15:08:36 +0300

    Update Bot.java and Score.java
    Score's constructor and two new methods have been added
    
    Bot's regularPlay method has been updated. This provides the bot to play smarter

--- Bot.java
+++ Bot.java
@@ -20,7 +20,47 @@ public class Bot extends Player {
         this.hand.remove(card_index);
         return card;
     }
-    private String regularPlay() {}
+    private String regularPlay(Score score, ArrayList<String> boardCards) {
+        // Calculate the total value of the cards on the board
+        int boardCardsValue = 0;
+        for (String card: boardCards) {
+            boardCardsValue += score.getCardPoint(card);
+        }
+        // Select a card in the hand if there is any valuable and suitable card to play
+        String lastCardOnBoard = boardCards.get(boardCards.size()-1);
+        String selectedCard = this.hand.get(0);
+        int selectedCardValue = score.getCardPoint(selectedCard);
+        int currentCardValue = 0;
+        boolean selected = false;
+        for (String card: this.hand) {
+            currentCardValue = score.getCardPoint(card);
+            if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) {
+                if (currentCardValue + boardCardsValue > 0) {
+                    selectedCard = card;
+                    selectedCardValue = currentCardValue;
+                    selected = true;
+                }
+            }
+            else if ((card.charAt(0) == lastCardOnBoard.charAt(0) || card.charAt(1) == lastCardOnBoard.charAt(1)) && currentCardValue > selectedCardValue) {
+                if (currentCardValue + boardCardsValue > 0) {
+                    selectedCard = card;
+                    selectedCardValue = currentCardValue;
+                    selected = true;
+                }
+            }
+        }
+        // Choose the card which has the lowest value if none of the cards is suitable
+        if (!selected) {
+            for (String card: this.hand) {
+                currentCardValue = score.getCardPoint(card);
+                if (currentCardValue < selectedCardValue) {
+                    selectedCard = card;
+                    selectedCardValue = currentCardValue;
+                }
+            }
+        }
+        return selectedCard;
+    }
     private String expertPlay() {}
     public String play() {
         String card = "";
--- Score.java
+++ Score.java
@@ -1,13 +1,19 @@
 import java.util.ArrayList;
 
 public class Score {
-    private ArrayList<String> cardsValues = new ArrayList<String>(); //valuable cards specified in points.txt
+    private ArrayList<String> cardsValues; //valuable cards specified in points.txt
 
-    public static String[] readFile() {}
-    public int  getCardPoint(String card){
-        String valuable_card  = null;    //one of cards in cardvalues
+    public Score(String path) {
+        this.cardsValues = new ArrayList<String>();
+        this.setCardValues(readFile(path));
+    }
+
+    public static String[] readFile(String path) {}
+    public void setCardValues(String[] points) {}
+    public int getCardPoint(String card){
+        String valuable_card = "";    //one of cards in cardvalues
         int card_value = 0;    //value of that card.
-        for(String i: cardsValues){
+        for(String i: this.cardsValues){
             valuable_card = i.split(" ")[0];
             card_value = Integer.parseInt(i.split(" ")[1]);
             if(card.equals(valuable_card)) return card_value;  
