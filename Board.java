96d1becde421a81cb2eaf522e6598a57ce95c89f
ngner046 <94244632+ngner046@users.noreply.github.com>
2023-05-08 22:27:44 +0300

    Update Board.java
    checkCards method has been implemented.

--- Board.java
+++ Board.java
@@ -11,4 +11,14 @@ public class Board {
 
     public void addCard(String card) {this.boardCards.add(card);}
     public void clearBoard() {this.boardCards.clear();}
+
+    public void checkCards(Player player, Board board, Score score) {
+        if (boardCards.size() > 1) {
+            if (this.boardCards.get(this.boardCards.size()-1).charAt(1) == 'J' || 
+                this.boardCards.get(this.boardCards.size()-1).charAt(1) == this.boardCards.get(this.boardCards.size()-2).charAt(1)) 
+            {
+                player.addtoStoredCards(board, score);
+            }
+        }
+    }
 }
