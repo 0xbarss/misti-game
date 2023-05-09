7bdbdcc4a72bd530bb652389b31b539188a4ceae
ngner046 <94244632+ngner046@users.noreply.github.com>
2023-05-09 18:47:24 +0300

    Update Bot.java
    regularPlay and expertPlay methods have been updated.

--- Bot.java
+++ Bot.java
@@ -30,41 +30,59 @@ public class Bot extends Player {
         return card;
     }
     private String regularPlay(Score score, ArrayList<String> boardCards) {
+        String selectedCard = this.hand.get(0);
+        int selectedCardValue = score.getCardPoint(selectedCard);
         // Calculate the total value of the cards on the board
         int boardCardsValue = 0;
         for (String card: boardCards) {
             boardCardsValue += score.getCardPoint(card);
         }
-        // Select a card in the hand if there is any valuable and suitable card to play
-        String lastCardOnBoard = boardCards.get(boardCards.size()-1);
-        String selectedCard = this.hand.get(0);
-        int selectedCardValue = score.getCardPoint(selectedCard);
-        int currentCardValue = 0;
-        boolean selected = false;
-        for (String card: this.hand) {
-            currentCardValue = score.getCardPoint(card);
-            if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) { // Play J if it exists and has more points than others
-                if (currentCardValue + boardCardsValue > 0) { // Select the card if the total value of the cards on the board is positive
-                    selectedCard = card;
-                    selectedCardValue = currentCardValue;
-                    selected = true;
+        // If there is at least 1 card on the board
+        if (boardCards.size() > 1) {
+            int currentCardValue = 0;
+            boolean selected = false;
+            String lastCardOnBoard = boardCards.get(boardCards.size()-1);
+            // Select a card in the hand if there is any valuable and suitable card to play
+            for (String card: this.hand) {
+                currentCardValue = score.getCardPoint(card);
+                // Play J if it exists and has more points than others
+                if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) {
+                    // Select the card if the total value of the cards on the board is positive
+                    if (currentCardValue + boardCardsValue > 0) { 
+                        selectedCard = card;
+                        selectedCardValue = currentCardValue;
+                        selected = true;
+                    }
+                }
+                // Play a suitable card if it has more points than others
+                else if (card.charAt(1) == lastCardOnBoard.charAt(1) && currentCardValue > selectedCardValue) {
+                    // Select the card if the total value of the cards on the board is positive
+                    if (currentCardValue + boardCardsValue > 0) {
+                        selectedCard = card;
+                        selectedCardValue = currentCardValue;
+                        selected = true;
+                    }
                 }
             }
-            else if (card.charAt(1) == lastCardOnBoard.charAt(1) && currentCardValue > selectedCardValue) { // Play a suitable card if it has more points than others
-                if (currentCardValue + boardCardsValue > 0) { // Select the card if the total value of the cards on the board is positive
-                    selectedCard = card;
-                    selectedCardValue = currentCardValue;
-                    selected = true;
+            // Choose the card which has the lowest value if none of the cards is suitable
+            if (!selected) {
+                for (String card: this.hand) {
+                    currentCardValue = score.getCardPoint(card);
+                    if (currentCardValue < selectedCardValue) {
+                        selectedCard = card;
+                        selectedCardValue = currentCardValue;
+                    }
                 }
             }
         }
-        // Choose the card which has the lowest value if none of the cards is suitable
-        if (!selected) {
+        else {
+            // Select a random card in the hand except for J
+            // If all of the cards are J, select first card
+            selectedCard = this.hand.get(0);
             for (String card: this.hand) {
-                currentCardValue = score.getCardPoint(card);
-                if (currentCardValue < selectedCardValue) {
+                if (card.charAt(1) != 'J') {
                     selectedCard = card;
-                    selectedCardValue = currentCardValue;
+                    break;
                 }
             }
         }
@@ -72,49 +90,65 @@ public class Bot extends Player {
         return selectedCard;
     }
     private String expertPlay(Score score, ArrayList<String> boardCards) {
+        String selectedCard = this.hand.get(0);
+        int selectedCardValue = score.getCardPoint(selectedCard);
         // Calculate the total value of the cards on the board
         int boardCardsValue = 0;
         for (String card: boardCards) {
             boardCardsValue += score.getCardPoint(card);
         }
-        // Select a card in the hand if there is any valuable and suitable card to play
-        String lastCardOnBoard = boardCards.get(boardCards.size()-1);
-        String selectedCard = this.hand.get(0);
-        int selectedCardValue = score.getCardPoint(selectedCard);
-        int currentCardValue = 0;
-        boolean selected = false;
-        for (String card: this.hand) {
-            currentCardValue = score.getCardPoint(card);
-            if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) { // Play J if it exists and has more points than others
-                if (currentCardValue + boardCardsValue > 0) { // Select the card if the total value of the cards on the board is positive
-                    selectedCard = card;
-                    selectedCardValue = currentCardValue;
-                    selected = true;
+        // If there is at least 1 card on the board
+        if (boardCards.size() > 1) {
+            int currentCardValue = 0;
+            boolean selected = false;
+            String lastCardOnBoard = boardCards.get(boardCards.size()-1);
+            // Select a card in the hand if there is any valuable and suitable card to play
+            for (String card: this.hand) {
+                currentCardValue = score.getCardPoint(card);
+                // Play J if it exists and has more points than others
+                if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) {
+                    // Select the card if the total value of the cards on the board is positive
+                    if (currentCardValue + boardCardsValue > 0) { 
+                        selectedCard = card;
+                        selectedCardValue = currentCardValue;
+                        selected = true;
+                    }
+                }
+                // Play a suitable card if it has more points than others
+                else if (card.charAt(1) == lastCardOnBoard.charAt(1) && currentCardValue > selectedCardValue) {
+                    // Select the card if the total value of the cards on the board is positive
+                    if (currentCardValue + boardCardsValue > 0) {
+                        selectedCard = card;
+                        selectedCardValue = currentCardValue;
+                        selected = true;
+                    }
                 }
             }
-            else if (card.charAt(1) == lastCardOnBoard.charAt(1) && currentCardValue > selectedCardValue) { // Play a suitable card if it has more points than others
-                if (currentCardValue + boardCardsValue > 0) { // Select the card if the total value of the cards on the board is positive
-                    selectedCard = card;
-                    selectedCardValue = currentCardValue;
-                    selected = true;
+            // Choose the card which has the lowest value if none of the cards is suitable
+            // And the card's rank has been played less until now
+            int currentRankCount = 0;
+            int LowestRankCount = 0;
+            if (!selected) {
+                for (String card: this.hand) {
+                    currentRankCount = 0;
+                    for (String mCard: this.memory) {
+                        if (mCard.charAt(1) == card.charAt(1)) currentRankCount++;
+                    }
+                    if (currentRankCount < LowestRankCount) {
+                        selectedCard = card;
+                        LowestRankCount = currentRankCount;
+                    }
                 }
             }
         }
-        // Choose the card which has the lowest value if none of the cards is suitable
-        // And the selected suit has been played less until now (<%50)
-        int suitCount = 0;
-        if (!selected) {
+        else {
+            // Select a random card in the hand except for J
+            // If all of the cards are J, select first card
+            selectedCard = this.hand.get(0);
             for (String card: this.hand) {
-                currentCardValue = score.getCardPoint(card);
-                if (currentCardValue < selectedCardValue) {
-                    suitCount = 0;
-                    for (String mcard: this.memory) {
-                        if (card.charAt(0) == mcard.charAt(0)) suitCount++;
-                    }
-                    if (suitCount < 7) {
-                        selectedCard = card;
-                        selectedCardValue = currentCardValue;
-                    }
+                if (card.charAt(1) != 'J') {
+                    selectedCard = card;
+                    break;
                 }
             }
         }
