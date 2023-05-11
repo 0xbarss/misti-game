cae1e8f99eb0ebab84214fb7a0ea6a79f461b9c7
ngner046 <94244632+ngner046@users.noreply.github.com>
2023-05-12 00:00:11 +0300

    Update Bot.java
    memory array has converted to static attribute
    
    expertPlay method has been updated

--- Bot.java
+++ Bot.java
@@ -4,31 +4,30 @@ import java.util.ArrayList;
 
 public class Bot extends Player {
     private String level;
-    private ArrayList<String> memory;
+    private static ArrayList<String> memory = new ArrayList<String>();
 
     public Bot(String nickname, String level) {
         super(nickname);
         this.level = level;
-        if (this.level.equals("E")) {
-            this.memory = new ArrayList<String>();
-        }
     }
 
+    public static void addToMemory(String card) {memory.add(card);}
+
+    @Override
     public String getLevel() {
         if (this.level.equals("N")) return "Novice";
         if (this.level.equals("R")) return "Regular";
         if (this.level.equals("E")) return "Expert";
         return "";
     }
-    public void addToMemory(String card) {this.memory.add(card);}
     
     private String novicePlay() {
         Random r = new Random(System.currentTimeMillis());
         int card_index = r.nextInt(this.hand.size());
         String card = this.hand.get(card_index);
-        this.hand.remove(card_index);
         return card;
     }
+
     private String regularPlay(Score score, ArrayList<String> boardCards) {
         String selectedCard = this.hand.get(0);
         int selectedCardValue = score.getCardPoint(selectedCard);
@@ -86,89 +85,52 @@ public class Bot extends Player {
                 }
             }
         }
-        this.hand.remove(selectedCard);
         return selectedCard;
     }
-    private String expertPlay(Score score, ArrayList<String> boardCards) {
-        String selectedCard = this.hand.get(0);
-        int selectedCardValue = score.getCardPoint(selectedCard);
-        // Calculate the total value of the cards on the board
-        int boardCardsValue = 0;
-        for (String card: boardCards) {
-            boardCardsValue += score.getCardPoint(card);
-        }
-        // If there is at least 1 card on the board
-        if (boardCards.size() > 1) {
-            int currentCardValue = 0;
-            boolean selected = false;
-            String lastCardOnBoard = boardCards.get(boardCards.size()-1);
-            // Select a card in the hand if there is any valuable and suitable card to play
-            for (String card: this.hand) {
-                currentCardValue = score.getCardPoint(card);
-                // Play J if it exists and has more points than others
-                if (card.charAt(1) == 'J' && currentCardValue > selectedCardValue) {
-                    // Select the card if the total value of the cards on the board is positive
-                    if (currentCardValue + boardCardsValue > 0) { 
-                        selectedCard = card;
-                        selectedCardValue = currentCardValue;
-                        selected = true;
-                    }
-                }
-                // Play a suitable card if it has more points than others
-                else if (card.charAt(1) == lastCardOnBoard.charAt(1) && currentCardValue > selectedCardValue) {
-                    // Select the card if the total value of the cards on the board is positive
-                    if (currentCardValue + boardCardsValue > 0) {
-                        selectedCard = card;
-                        selectedCardValue = currentCardValue;
-                        selected = true;
-                    }
-                }
+
+    private String expertPlay(Score score, ArrayList<String> boardCards, String selectedCard) {   
+        // Choose the card which has the lowest value if none of the cards is suitable
+        // And the card's rank has been played less until now
+        // The card has not to be J
+        int currentCardValue = 0;
+        int currentRankCount = 0;
+        int lowestRankCount = 0;
+        int lowestCardValue = 0;
+        for (String card: this.hand) {
+            if (card.charAt(1) == 'J') continue;
+            currentRankCount = 0;
+            currentCardValue = score.getCardPoint(card);
+            for (String mCard: memory) {
+                if (mCard.charAt(1) == card.charAt(1)) currentRankCount++;
             }
-            // Choose the card which has the lowest value if none of the cards is suitable
-            // And the card's rank has been played less until now
-            int currentRankCount = 0;
-            int LowestRankCount = 0;
-            if (!selected) {
-                for (String card: this.hand) {
-                    currentRankCount = 0;
-                    for (String mCard: this.memory) {
-                        if (mCard.charAt(1) == card.charAt(1)) currentRankCount++;
-                    }
-                    if (currentRankCount < LowestRankCount) {
-                        selectedCard = card;
-                        LowestRankCount = currentRankCount;
-                    }
-                }
+            if (currentRankCount < lowestRankCount && currentCardValue < lowestCardValue) {
+                selectedCard = card;
+                lowestRankCount = currentRankCount;
+                lowestCardValue = currentCardValue;
             }
         }
-        else {
-            // Select a random card in the hand except for J
-            // If all of the cards are J, select first card
-            selectedCard = this.hand.get(0);
-            for (String card: this.hand) {
-                if (card.charAt(1) != 'J') {
-                    selectedCard = card;
-                    break;
-                }
-            }
-        }
-        this.hand.remove(selectedCard);
         return selectedCard;
     }
+    
     @Override
     public String play(Scanner sc, Score score, ArrayList<String> boardCards) {
         String card = "";
         switch (this.level) {
             case "N":
                 card = novicePlay();
+                this.hand.remove(card);
                 break;
             case "R":
                 card = regularPlay(score, boardCards);
+                this.hand.remove(card);
                 break;
             case "E":
-                card = expertPlay(score, boardCards);
+                card = regularPlay(score, boardCards);
+                card = expertPlay(score, boardCards, card);
+                this.hand.remove(card);
                 break;
         }
+        System.out.println(this.nickname + " played " + card);
         return card;
     }
 }
