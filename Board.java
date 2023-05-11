0bddde5c45f736d562a66a25c20ae22e7c274639
Kubilay <kubilay.zengin@std.ieu.edu.tr>
2023-05-12 00:14:19 +0300

    Latest version of the Board Class (getHandString added , getLastCard added,writeHand added, addCard method updated, checkCards method changed)

--- Board.java
+++ Board.java
@@ -1,23 +1,68 @@
 import java.util.ArrayList;
 
 public class Board {
+    private static int handCount = 0;
+    private static int moveCount = 0;
+    private static int playedCardsCount = 0;
+    private static String handString = "";
+    private int numberOfPlayers;
     private ArrayList<String> boardCards;
+    private boolean verbosenessMode;
 
-    public Board() {
+    public Board(int numberOfPlayers, boolean verbosenessMode) {
+        this.numberOfPlayers = numberOfPlayers;
+        this.verbosenessMode = verbosenessMode;
         this.boardCards = new ArrayList<String>();
     }
 
+    public String getHandString() {return handString;}
     public ArrayList<String> getBoardCards() {return this.boardCards;}
+    public String getLastCard() {
+        if (this.boardCards.size() > 0) return this.boardCards.get(this.boardCards.size()-1);
+        return "";
+    }
 
-    public void addCard(String card) {this.boardCards.add(card);}
+    public void addCard(String card) {
+        Bot.addToMemory(card);
+        this.boardCards.add(card);
+    }
     public void clearBoard() {this.boardCards.clear();}
 
     public void checkCards(Player player, Board board, Score score) {
+        playedCardsCount++;
+        // Write the played card to string
+        if (playedCardsCount % this.numberOfPlayers == 1 && this.verbosenessMode) {
+            moveCount++;
+            handString += "\n" + moveCount + ".";
+        }
+        // If there are at least 2 cards on the board, check matches
         if (boardCards.size() > 1) {
-            if (this.boardCards.get(this.boardCards.size()-1).charAt(1) == 'J' || 
-                this.boardCards.get(this.boardCards.size()-1).charAt(1) == this.boardCards.get(this.boardCards.size()-2).charAt(1)) 
+            if (this.boardCards.get(this.boardCards.size()-1).charAt(1) == 'J' ||
+                    this.boardCards.get(this.boardCards.size()-1).charAt(1) == this.boardCards.get(this.boardCards.size()-2).charAt(1))
             {
+                if (this.verbosenessMode) handString += " " + this.boardCards.get(this.boardCards.size()-1) + "!";
                 player.addtoStoredCards(board, score);
+            }
+            else if (boardCards.size() > 0 && this.verbosenessMode) handString += " " + this.boardCards.get(this.boardCards.size()-1);
+        }
+        else if (boardCards.size() > 0 && this.verbosenessMode) handString += " " + this.boardCards.get(this.boardCards.size()-1);
+    }
+
+    public void writeHand(ArrayList<Player> players) {
+        handCount++;
+        moveCount = 0;
+        handString = "\nHand " + handCount;
+        Player player = null;
+        if (this.verbosenessMode) {
+            for (int i=0; i<players.size(); i++) {
+                player = players.get(i);
+                handString += "; Player-" + (i+1) + ": " + player.getHand().toString() + " Score: " + player.getScore();
+            }
+        }
+        else {
+            for (int i=0; i<players.size(); i++) {
+                player = players.get(i);
+                handString += "; Player-" + (i+1) + ": " + " Score: " + player.getScore();
             }
         }
     }
