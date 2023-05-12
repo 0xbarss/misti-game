719fa675395391a8ee200b6703051193878de125
YumrukPaketcisi <kbasarkocabas@gmail.com>
2023-05-12 19:56:04 +0300

    Deck
    Made some litte changes.

--- Deck.java
+++ Deck.java
@@ -10,13 +10,11 @@ public class Deck {
     public Deck() {
         deck = new ArrayList<String>();
         createDeck();
-        Collections.shuffle(deck);
-        cutDeck();
     }
     
     public ArrayList<String> getDeck() {return deck;}
     
-    public void createDeck() {
+    private void createDeck() {
         for(char s: suits){
             for(char r: ranks){
                 String card = ""+s+r;
@@ -24,8 +22,13 @@ public class Deck {
             }
         }
     }
-    // Collections.shuffle() - Shuffle
-    private void cutDeck() {
+
+    public void shuffleDeck() {
+        Collections.shuffle(deck);
+        System.out.println("The cards are shuffled.");
+    }
+
+    public void cutDeck() {
         Random r = new Random(System.currentTimeMillis());
         ArrayList<String> newdeck = new ArrayList<String>();
         int size = deck.size();
@@ -33,15 +36,19 @@ public class Deck {
         for(int i = 0; i<size; i++){
             newdeck.add(this.deck.get(i<size-cutIndex ? cutIndex+i: i-(size-cutIndex)));
         }
-        this.deck = newdeck;    
+        this.deck = newdeck;  
+        System.out.println("The cards are cut.");  
     }
 
     public void dealCards(ArrayList<Player> players, Board board) {
+        shuffleDeck();
+        cutDeck();
         if (deck.size() == 52) {
             for (int i=0; i<4; i++) {
                 board.addCard(deck.get(deck.size()-1));
                 deck.remove(deck.size()-1);
             }
+            System.out.println("4 cards are placed on the board.");
         }
         for (int i=0; i<4; i++) {
             for (Player player: players) {
@@ -49,5 +56,6 @@ public class Deck {
                 deck.remove(deck.size()-1);
             }
         }
+        System.out.println("4 cards are dealt to each player.");
     }
 }
